14f461bee4485ce04944e68f921c841bd6879a7f
Simone Masoero <smsimone65@gmail.com>
2025-01-26 18:37:39 +0100

    aligned buttons in a little nicer way

--- Leader Key/GeneralPane.swift
+++ Leader Key/GeneralPane.swift
@@ -26,29 +26,33 @@ struct GeneralPane: View {
           )
 
           HStack {
-            Button("Save to file") {
-              config.saveConfig()
-            }
+              VStack(alignment: .leading) {
+                  Button("Save to file") {
+                      config.saveConfig()
+                  }
+                  
+                  Button("Reload from file") {
+                      config.reloadConfig()
+                  }
+              }
 
-            Button("Reload from file") {
-              config.reloadConfig()
-            }
-
-            Button("Reveal config file in Finder") {
-              NSWorkspace.shared.activateFileViewerSelecting([config.fileURL()])
-            }
-              
-            Button("Change config file location"){
-                let panel = NSOpenPanel()
-                panel.allowsMultipleSelection = false
-                panel.canChooseDirectories = true
-                panel.canChooseFiles = true
-                if panel.runModal() != .OK{
-                    return
-                }
-                guard let selectedPath = panel.url else { return }
-                config.changeConfigLocation(selectedPath)
-            }
+              VStack{
+                  Button("Reveal config file in Finder") {
+                      NSWorkspace.shared.activateFileViewerSelecting([config.fileURL()])
+                  }
+                  
+                  Button("Change config file location"){
+                      let panel = NSOpenPanel()
+                      panel.allowsMultipleSelection = false
+                      panel.canChooseDirectories = true
+                      panel.canChooseFiles = true
+                      if panel.runModal() != .OK{
+                          return
+                      }
+                      guard let selectedPath = panel.url else { return }
+                      config.changeConfigLocation(selectedPath)
+                  }
+              }
           }
         }
       }
